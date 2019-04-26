cac05162223652181cc7811ee6e737e55b7d814d
Luísa Barros <qlullyq@gmail.com>
2019-04-26 17:47:52 -0300

    Add form view

--- gonode/modulo1/index.js
+++ gonode/modulo1/index.js
@@ -1,31 +1,27 @@
-const express = require("express");
-const nunjucks = require("nunjucks");
+const express = require('express');
+const nunjucks = require('nunjucks');
 
 const app = express();
 
-nunjucks.configure("views", {
+nunjucks.configure('views', {
   autoescape: true,
   express: app,
-  watch: true
+  watch: true,
 });
 
 app.use(express.urlencoded({ extended: false }));
 
-app.set("view engine", "njk");
+app.set('view engine', 'njk');
 
-const users = ["Luísa Barros", "Dani Alcântara", "André Santos"];
+const users = ['Luísa Barros', 'Danielle Alcântara', 'André Santos'];
 
-app.get("/", (req, res) => {
-  return res.render("list", { users });
-});
+app.get('/', (req, res) => res.render('list', { users }));
 
-app.get("/new", (req, res) => {
-  return res.render("new");
-});
+app.get('/new', (req, res) => res.render('new'));
 
-app.post("/create", (req, res) => {
-  console.log(req.body);
-  return res.send("okay");
+app.post('/create', (req, res) => {
+  users.push(req.body.user);
+  return res.redirect('/');
 });
 
 app.listen(3000);
